cf8fbc1067ea5d063976615d66f98705b0748d00
unknown <denzell22@hotmail.com>
2022-02-11 22:27:07 -0300

    att js

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ function entrada(num)   {
    var numero =  document.getElementById("resultado").innerHTML;
    document.getElementById('resultado').innerHTML = numero + num
 }
+function apagar(){
+   var resultado = document.getElementById('resultado').innerHTML;
+   document.getElementById('resultado').innerHTML = resultado.replace(/.$/,'')
+}
+function limpar(){
+   document.getElementById('resultado').innerHTML=""
+}
+function resposta(){
+   var resultado = document.getElementById('resultado').innerHTML;
+   if(resultado){
+   document.getElementById('resultado').innerHTML=eval(resultado)
+   }
+   else{
+      document.getElementById('resultado').innerHTML=":/"
+   }
+}
