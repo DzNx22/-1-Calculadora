65f39a9336d68df24d3e72f1fe6782cdee52ad87
unknown <denzell22@hotmail.com>
2022-02-15 19:11:12 -0300

    corbug

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function entrada(num)   {
+function entrada(num){
 
    var numero =  document.getElementById("resultado").innerHTML;
    document.getElementById('resultado').innerHTML = numero + num
